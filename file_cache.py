e64d27f938e07f37e2811dd92a27d070d5abb8b2
Loren Gordon <lorengordon@users.noreply.github.com>
2018-05-02 07:38:17 -0400

    Simplifies file cache wrapper around py-getter

--- file_cache.py
+++ file_cache.py
@@ -3,18 +3,12 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals, with_statement)
 
-import json
 import sys
 
 import py_getter
 import tf_external
 
-
-def main(**kwargs):
-    return py_getter.main(**kwargs)
-
-
 if __name__ == "__main__":
     tfe = tf_external.TfExternal()
     args = tfe.query_args(sys.stdin)
-    sys.exit(tfe.out_json(main(**args)))
+    sys.exit(tfe.out_json(py_getter.main(**args)))
