5d03b8ac4ec9aad6936c6ebc83d44600f75f38b1
Juno Yeon <73125906+kumoy@users.noreply.github.com>
2021-01-08 17:20:50 -0500

    Lint Terraform and python files.

--- tf_external/__init__.py
+++ tf_external/__init__.py
@@ -11,7 +11,7 @@ from __future__ import (
 import json
 
 
-class TfExternal(object):
+class TfExternal(object):  # pylint: disable=useless-object-inheritance
     """Wrap Terraform External provider."""
 
     @staticmethod
